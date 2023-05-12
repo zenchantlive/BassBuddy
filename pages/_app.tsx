b79c1dc0f1b34ffe621fa243f7f476cd1678c390
zenchantlive <103866469+zenchantlive@users.noreply.github.com>
2023-05-11 22:18:39 -0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 /* global.css */
-@import url('https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;600;700&display=swap');
+import url('https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;600;700&display=swap');
 
 .font-space-grotesk {
   font-family: 'Space Grotesk', sans-serif;
