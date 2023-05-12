b5b7243236262784671b96786e98e7fa1d9205e8
zenchantlive <103866469+zenchantlive@users.noreply.github.com>
2023-05-11 22:43:27 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -95,7 +95,6 @@ const Home: NextPage = () => {
         </Card>
         <button
           className={classNames(
-            ## spaceGrotesk.className, ##
             "text-white rounded-xl px-5 py-2 m-5 text-xl font-bold hover:opacity-70 transition-all duration-300 disabled:opacity-50"
           )}
           style={{ background: client.appThemeColor }}
