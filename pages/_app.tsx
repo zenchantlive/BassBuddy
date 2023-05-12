7e558ad0a36d6982319c15581d0c6c0c8209b4ab
zenchantlive <103866469+zenchantlive@users.noreply.github.com>
2023-05-11 22:24:18 -0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-/* global.css */
 import url('https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;600;700&display=swap');
 
 .font-space-grotesk {
