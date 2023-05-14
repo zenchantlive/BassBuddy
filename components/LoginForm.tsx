a7c468f7cb981cc5a6af79b7f1fb632b76138abc
zenchantlive <jordanlive121@gmail.com>
2023-05-14 10:26:58 -0700

    commit

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -1,23 +1,46 @@
-// src/components/LoginForm.tsx
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const LoginForm = () => {
+const LoginForm: React.FC = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
-  const login = async (event: React.FormEvent) => {
-    event.preventDefault();
-    const res = await axios.post('/login', { email, password });
-    // Save the token to local storage
-    localStorage.setItem('token', res.data.token);
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/api/login`, {
+        email,
+        password,
+      });
+
+      // Save the JWT in local storage
+      localStorage.setItem('token', response.data.token);
+
+      // Redirect the user to the home page (or wherever you want)
+      // Use Next.js's useRouter hook for this
+    } catch (err: any) {
+      setError(err?.response?.data?.message || 'An error occurred');
+    }
   };
 
   return (
-    <form onSubmit={login}>
-      <input type="email" value={email} onChange={e => setEmail(e.target.value)} required title="Enter your email address" />
-      <input type="password" value={password} onChange={e => setPassword(e.target.value)} required title="Enter your password" />
-      <button type="submit">Login</button>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Email"
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+      />
+      <button type="submit">Log In</button>
+      {error && <p>{error}</p>}
     </form>
   );
 };
