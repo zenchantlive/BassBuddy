30245261eb8db67169eddb3806eb8d5b326ed0a1
zenchantlive <jordanlive121@gmail.com>
2023-05-15 14:24:29 -0700

    commit

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,9 +1,16 @@
 // pages/login.tsx
 import React from 'react';
 import LoginForm from '../components/LoginForm';
+import Link from 'next/link';
 
 const Login = () => {
-  return <LoginForm />;
+  return (
+    <div>
+      <LoginForm />
+      <p>Don't have an account? <Link href="/register"><a>Register here</a></Link></p>
+    </div>
+  );
 };
 
 export default Login;
+
