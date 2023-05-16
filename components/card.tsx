eb6f7a5af34797b57b7da994e9fff9bc576a7853
zenchantlive <jordanlive121@gmail.com>
2023-05-15 19:21:00 -0700

    commit

--- components/card.tsx
+++ components/card.tsx
@@ -1,22 +1,20 @@
-import { ReactNode, type CSSProperties } from "react";
+import { ReactNode } from "react";
 import classNames from "classnames";
+import "../styles/card.css";
 
 export default function Card({
   children,
   className,
-  style,
 }: {
   className?: string;
   children: ReactNode;
-  style?: CSSProperties;
 }) {
   return (
     <div
       className={classNames(
-        "isolate rounded-xl border border-gray-600/10 shadow-xl shadow-gray-400/10 transition-all duration-300 dark:shadow-black/0",
+        "card",
         className
       )}
-      style={style}
     >
       {children}
     </div>
